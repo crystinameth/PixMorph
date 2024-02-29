57be09804d6c86dff0fd592278aa9a021ff3cc72
shivimishra <shivimishra130103@gmail.com>
2024-02-29 18:23:36 +0530

    transformation header

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import { transformationTypes } from '@/constants'
+import Header from '@/components/shared/Header'
 
-const AddTransformationTypePage = () => {
+const AddTransformationTypePage = ({ params: { type } }: SearchParamProps) => {
+  const transformation = transformationTypes[type];
+
   return (
-    <div>AddTransformationTypePage</div>
+    <Header
+      title={transformation.title} 
+      subtitle={transformation.subTitle}
+    />
   )
 }
 
